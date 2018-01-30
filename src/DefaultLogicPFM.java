bd32be3228a83d6b65adc2785eca227ac8735c0a
Punera <punanand@u705a0f8c477c5a252a55.ant.amazon.com>
2018-01-30 10:10:12 +0530

    Second Commit

--- src/DefaultLogicPFM.java
+++ src/DefaultLogicPFM.java
@@ -13,20 +13,45 @@ import java.util.*;
 
 public class DefaultLogicPFM {
 
-    private static Map<String, List<String>> defaultLogicMap;
+    private static Map<String, List<String>> defaultLogicTable;
+    private static Set<String> mSetOfSupportedPFM;
+    private static Set<String> mSetOfBetaSupportedPFM;
+    private static Set<String> mSetOfTentativeSupportedPFM;
+    private static Map<String, String> mapOfDefaultingLogic;
+    private static List<String> marketsLaunchOrder;
 
-    public static Map<String, List<String>> getDefaultLogicMap() {
-        return defaultLogicMap;
+    public static Set<String> getmSetOfSupportedPFM() {
+        return mSetOfSupportedPFM;
+    }
+
+    public static Set<String> getmSetOfBetaSupportedPFM() {
+        return mSetOfBetaSupportedPFM;
+    }
+
+    public static Set<String> getmSetOfTentativeSupportedPFM() {
+        return mSetOfTentativeSupportedPFM;
+    }
+
+    public static Map<String, String> getMapOfDefaultingLogic() {
+        return mapOfDefaultingLogic;
+    }
+
+    public static Map<String, List<String>> getDefaultLogicTable() {
+        return defaultLogicTable;
     }
 
     public DefaultLogicPFM() {
         JSONObject defaultJson;
-
+        mSetOfTentativeSupportedPFM = DefaultingLogicConstants.tentativeMarkets;
+        mSetOfSupportedPFM = DefaultingLogicConstants.currentMarkets;
+        marketsLaunchOrder = DefaultingLogicConstants.launchesOrder;
+        mSetOfBetaSupportedPFM = DefaultingLogicConstants.betaMarkets;
+        mapOfDefaultingLogic = new HashMap<>();
         try {
-            String text = new String(Files.readAllBytes(Paths.get("/home/local/ANT/punanand/DefaultLogic/src/main/logic.json")));
+            String text = new String(Files.readAllBytes(Paths.get("/home/local/ANT/punanand/DefaultingLogic/src/res/logic.json")));
             defaultJson = new JSONObject(text);
             Iterator<String> keys = defaultJson.keys();
-            defaultLogicMap = new LinkedHashMap<>();
+            defaultLogicTable = new LinkedHashMap<>();
             while (keys.hasNext()){
                 List<String> list = new ArrayList<>();
                 String key = keys.next();
@@ -34,9 +59,59 @@ public class DefaultLogicPFM {
                 for( int i=0; i<jsonArray.length();i++){
                     list.add(jsonArray.getString(i));
                 }
-                defaultLogicMap.put(key,list);
+                defaultLogicTable.put(key,list);
             }
         } catch (JSONException | IOException e) {
             throw new RuntimeException("Exception in reading defaults JSON", e); }
+    }
+
+    public void populateArtifacts() {
+        for (String pfm: marketsLaunchOrder) {
+            int count =0;
+            String pfmCurrent = pfm;
+            if( mSetOfSupportedPFM.contains(pfm)){
+                //System.out.println(pfmCurrent+ " defaults to "+ pfm);
+                continue;
+            }
+            else{
+                while (true){
+                    if(++count >= defaultLogicTable.size()){
+                        try {
+                            throw new Exception(" Defaulting Logic is wrong");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                    List<String> list= defaultLogicTable.get(pfm);
+                    if(count ==0 && marketsLaunchOrder.contains(pfm) == false){
+                        try {
+                            throw new Exception("Something wrong with this pfm "+ pfm);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                    else if(mSetOfSupportedPFM.contains(list.get(marketsLaunchOrder.indexOf(pfm)))){
+                        mapOfDefaultingLogic.put(pfmCurrent, list.get(marketsLaunchOrder.indexOf(pfm)));
+                        //System.out.println(pfmCurrent+" defaults to "+ list.get(marketsLaunchOrder.indexOf(pfm)));
+                        break;
+                    }
+                    else if(count>0 || pfm == list.get(marketsLaunchOrder.indexOf(pfm))){
+                        int index = marketsLaunchOrder.indexOf(pfm);
+                        while(index >=0 && mSetOfSupportedPFM.contains(list.get(index)) == false){
+                            index--;
+                        }
+                        //System.out.println(pfmCurrent+" defaults to "+list.get(index));
+                        mapOfDefaultingLogic.put(pfmCurrent, list.get(index));
+                        break;
+                    }
+                    else {
+                        pfm = list.get(marketsLaunchOrder.indexOf(pfm));
+                        continue;
+                    }
+                }
+            }
+        }
     }
 }
--- src/DefaultingLogicConstants.java
+++ src/DefaultingLogicConstants.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 final public class DefaultingLogicConstants {
-    public static final List<String> launchesOrder = new ArrayList<String>(Arrays.asList("US","DE","IN","JP","CA","AU","FR","IT","ES","MX","CN","BR"));
-    public static final List<String> currentMarkets = new ArrayList<String>(Arrays.asList("US","MX","FR","IN"));
+    public static final List<String> launchesOrder = new ArrayList<String>(Arrays.asList("US","UK","DE","IN","JP","CA","AU","FR","IT","ES","MX","CN","BR"));
+    public static final Set<String> currentMarkets = new LinkedHashSet<>(Arrays.asList("US","CN","ES","UK"));
+    public static final Set<String> betaMarkets = new LinkedHashSet<>(Arrays.asList("ES","IT"));
+    public static final Set<String> tentativeMarkets = new LinkedHashSet<>(Arrays.asList("ES","IT","JP","CA"));
 }
